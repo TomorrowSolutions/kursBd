90a6481dadd29663045b853a33917438e4358fac
WorkPC <WorkPC@KOMPUTER>
2022-11-23 14:47:23 +0300

    rdgdfg

--- login.cs
+++ login.cs
@@ -53,7 +53,7 @@ namespace kursBd
 
                 if (emplRadio.Checked)
                 {
-                   
+                   fegefge
                 }
                 else if (managerRadio.Checked)
                 {
